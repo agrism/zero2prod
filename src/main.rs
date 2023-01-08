1a782fee4f5810712d55f2cb0490ff8050c4647f
Agris <7924@inbox.lv>
2023-01-09 00:20:30 +0200

    refactor health_check test

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+use std::io::Result;
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    // let body = async move {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
-    // };
-    // tokio::runtime::Builder::new_multi_thread()
-    //     .enable_all()
-    //     .build()
-    //     .expected("Failed building the Runtime")
-    //     .block_on(body)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::health_check;
-
-    #[tokio::test]
-    async fn health_check_succeeds(){
-        let response = health_check().await;
-
-        assert!(response.status().is_success())
-    }
+async fn main() -> Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
