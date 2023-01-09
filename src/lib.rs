4c7c73ab06cab6b4d8d7ab26a54ff55a4c757fb2
Agris <7924@inbox.lv>
2023-01-10 00:40:11 +0200

    update test

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,17 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
