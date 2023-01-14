325285983bbe53ba24a7392735cfa4984ae89f38
Agris <7924@inbox.lv>
2023-01-14 15:15:41 +0200

    add connection to app_data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::io::Result;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -7,8 +8,11 @@ use zero2prod::startup::run;
 async fn main() -> Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     // We hae removed the hard-coded `800` - it's now coming from our settings!
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,19 @@
 use crate::routes::*;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::io::Result;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
