9fd4477a4fea839651e32d72310b43b37ad67720
Agris <7924@inbox.lv>
2023-01-14 17:37:15 +0200

    fix test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::io::Result;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -8,11 +8,10 @@ use zero2prod::startup::run;
 async fn main() -> Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
-    // We hae removed the hard-coded `800` - it's now coming from our settings!
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,19 +1,19 @@
 use crate::routes::*;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::io::Result;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server> {
-    let connection = web::Data::new(connection);
+pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server> {
+    let connection_pool = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
             .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection.clone())
+            .app_data(connection_pool.clone())
     })
     .listen(listener)?
     .run();
