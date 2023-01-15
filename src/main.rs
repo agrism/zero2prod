54f98e7ca0eb3cb89b1084025dd130c2d2f44364
Agris <7924@inbox.lv>
2023-01-15 15:27:30 +0200

    add logger middleware

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::io::Result;
 use std::net::TcpListener;
@@ -6,6 +7,10 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    //  `init` does call `set_logger`, so this is all we need to do.
+    // We ara falling back printing all logs at info-level or above
+    // if the RUST_LOG environment variable gas not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::routes::*;
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::io::Result;
@@ -9,6 +10,8 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server> {
     let connection_pool = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
+            // Middleware are added using the `wrap` method on `App`
+            .wrap(Logger::default())
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
