59119a00dbcefe0616ef4c7e415afe77547a255a
Agris <7924@inbox.lv>
2023-01-19 23:34:27 +0200

    refactor

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[allow(dead_code)]
@@ -11,25 +10,30 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
+
 pub async fn subscribe(
     form: web::Form<FormData>,
     // Retrieving a connection from the application state
-    connection_pool: web::Data<PgPool>,
+    pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let request_id = Uuid::new_v4();
+    // let request_id = Uuid::new_v4();
     // Spans, like logs, have an associated level
     // `info_span` creates a span at the info-level
-    let request_span = tracing::info_span!(
-        "Adding an new subscriber.",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    );
+    // let request_span = tracing::info_span!("Saving new subscriber details in the database",);
 
     // Using `enter` in an async function is a recipe for disaster!
     // Bear with me for now, but don't fo this at home.
     // See the following section on `Instrumenting Futures`
-    let _request_span_guard = request_span.enter();
+    // let _request_span_guard = request_span.enter();
     // `_request_span_guard` is dropped at the end of `subscribe`
     // That's when we "exit" the span
 
@@ -37,39 +41,38 @@ pub async fn subscribe(
     // `.instrument` takes care of it at the right moments
     // in the query future lifetime
 
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+    // let query_span = tracing::info_span!("Saving new subscriber details in the database");
 
-    match sqlx::query!(
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4);
+        VALUES ($1, $2, $3, $4)
         "#,
         sqlx::types::Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
-    // We use `get_ref` to get an immutable reference to the `PgConnection`
-    // wrapped by `web::Data`
-    .execute(connection_pool.get_ref())
-    // First we attach the instrumentation, then we `.await` it
-    .instrument(query_span)
+    .execute(pool)
     .await
-    {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+        // Using the `?` operator to return early
+        // if the function failed, returning a sqlx::Error
+        // We will talk about error handling in depth later!
+    })?;
+    Ok(())
 }
