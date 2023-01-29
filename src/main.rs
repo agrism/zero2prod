6691b8cdc4675b4285b0463e7419656dd2d7ac6a
Agris <7924@inbox.lv>
2023-01-29 13:17:42 +0200

    building docker image

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::io::Result;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -12,9 +12,12 @@ async fn main() -> Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres");
+
+    let connection_pool = PgPoolOptions::new()
+        .idle_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .unwrap();
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
