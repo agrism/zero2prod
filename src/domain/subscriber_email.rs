f836ea9834824a5813e33777a73e61c9f35160f5
Agris <7924@inbox.lv>
2023-02-05 00:58:33 +0200

    add faker for email testing

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -21,8 +21,13 @@ impl AsRef<str> for SubscriberEmail {
 
 #[cfg(test)]
 mod tests {
+    // We are importing the `SafeEmail` faker!
+    // We also need the `Fake` trait to get access to the
+    // `.fake` method on `SafeEmail`
     use super::SubscriberEmail;
     use claims::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
 
     #[test]
     fn empty_string_is_rejected() {
@@ -40,5 +45,11 @@ mod tests {
     fn email_missin_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn valid_emails_are_parsed_successfully() {
+        let email = SafeEmail().fake();
+        claims::assert_ok!(SubscriberEmail::parse(email));
     }
 }
