9d5ed03488a596cf42fc4eead82e93c37de8a424
Agris <7924@inbox.lv>
2023-02-05 00:50:31 +0200

    refactor domain and add email validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,4 @@
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 use unicode_segmentation::UnicodeSegmentation;
@@ -29,10 +29,12 @@ pub async fn subscribe(
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
 
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
     };
+
+    let new_subscriber = NewSubscriber { email, name };
 
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -54,7 +56,7 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         sqlx::types::Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         // Using `inner_ref`!
         new_subscriber.name.as_ref(),
         chrono::Utc::now()
