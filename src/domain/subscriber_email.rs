93825d083f399bc49283e058fc4cce8eff90c01d
Agris <7924@inbox.lv>
2023-02-05 01:30:41 +0200

    update email validation

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -29,11 +29,9 @@ mod tests {
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
 
-    #[test]
-    fn empty_string_is_rejected() {
-        let email = "".to_string();
-        assert_err!(SubscriberEmail::parse(email));
-    }
+    // Both `Clone` and `Debug` are required by `quickcheck`
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
 
     #[test]
     fn email_missing_at_symbol_is_rejected() {
@@ -48,8 +46,21 @@ mod tests {
     }
 
     #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        claims::assert_ok!(SubscriberEmail::parse(email));
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        dbg!(&valid_email.0);
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
