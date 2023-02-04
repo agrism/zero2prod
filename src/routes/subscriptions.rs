a480511f2e9aa79409636d2379e35fdf2553e044
Agris <7924@inbox.lv>
2023-02-04 13:37:15 +0200

    add name validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
@@ -22,6 +23,9 @@ pub async fn subscribe(
     // Retrieving a connection from the application state
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish();
+    }
     // let request_id = Uuid::new_v4();
     // Spans, like logs, have an associated level
     // `info_span` creates a span at the info-level
@@ -72,4 +76,12 @@ pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sql
         // We will talk about error handling in depth later!
     })?;
     Ok(())
+}
+
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+    let is_too_long = s.graphemes(true).count() > 256;
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
